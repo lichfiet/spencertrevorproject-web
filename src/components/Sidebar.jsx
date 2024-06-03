039c84eb823213ab5a5042f026ecf06352c39cf0
lichfiet <sortaminty@gmail.com>
2024-06-02 23:09:34 -0600

    layout

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,16 +8,20 @@ const Sidebar = () => {
             <Grid container spacing={2}>
                 <Grid xs={4}>
                     <Box my={2} display="flex" alignItems="center" sx={{ border: '2px solid grey' }}>
-                        <Button variant="contained">Contained</Button>
+                        <Button variant="contained">Info</Button>
                     </Box>
                 </Grid>
                 <Grid xs={8}>
                     <Box my={2} display="flex" alignItems="center" sx={{ border: '2px solid grey' }}>
-                        <Button variant="contained">Contained</Button>
+                        <Button variant="contained">More Info</Button>
                     </Box>
                 </Grid>
             </Grid>
-            <Button variant="contained">Contained</Button>
+            <Button variant="contained">Upgrade1</Button>
+            <Button variant="contained">Upgrade2</Button>
+            <Button variant="contained">Upgrade3</Button>
+            <Button variant="contained">Upgrade5</Button>
+            <Button variant="contained">Upgrade6</Button>
 
         </Box>
     );
