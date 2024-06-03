f1a541fcd03256997567181d135044c78f0a6faa
lichfiet <sortaminty@gmail.com>
2024-06-02 23:07:55 -0600

    mepw

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
 import { Box, Button } from '@mui/material';
+import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
 
 const Sidebar = () => {
     return (
-      <Box sx={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
-        <Button variant="contained">Contained</Button>
-        <Button variant="contained">Contained</Button>
-        <Button variant="contained">Contained</Button>
-        <Button variant="contained">Contained</Button>
-        <Button variant="contained">Contained</Button>
-        <Button variant="contained">Contained</Button>
-      </Box>
+        <Box sx={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+            <Grid container spacing={2}>
+                <Grid xs={4}>
+                    <Box my={2} display="flex" alignItems="center" sx={{ border: '2px solid grey' }}>
+                        <Button variant="contained">Contained</Button>
+                    </Box>
+                </Grid>
+                <Grid xs={8}>
+                    <Box my={2} display="flex" alignItems="center" sx={{ border: '2px solid grey' }}>
+                        <Button variant="contained">Contained</Button>
+                    </Box>
+                </Grid>
+            </Grid>
+            <Button variant="contained">Contained</Button>
+
+        </Box>
     );
-  };
-  
+};
+
 
 export default Sidebar;
